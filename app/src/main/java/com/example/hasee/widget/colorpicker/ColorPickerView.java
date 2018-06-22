95bb8bf578861c8a2b1b29f46cf91526c064ca84
hujin <hujin@khlcn.com>
2018-06-22 16:01:53 +0800

    6.22

---
+++ app/src/main/java/com/example/hasee/widget/colorpicker/ColorPickerView.java
@@ -0,0 +1,39 @@
+package com.example.hasee.widget.colorpicker;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Demo ${CLASS}
+ *
+ * @author TT
+ * @date 2018/6/22 12:00
+ */
+
+public class ColorPickerView extends View {
+    public ColorPickerView(Context context) {
+        super(context);
+        initView();
+    }
+
+    public ColorPickerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public ColorPickerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    public ColorPickerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
